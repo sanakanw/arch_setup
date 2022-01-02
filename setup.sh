2aa45416a589e4ef6d32fbfde8d3b9e5a08482ff
sanakan <xdkekekek@gmail.com>
2022-01-02 15:27:17 +1100

    im stuff

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo pacman -S pulseaudio pulseaudio-alsa alsa-utils
 
 sudo pacman -S git python python2 python-pip base-devel nmap
 
-sudo pacman -S xorg xorg-xinit
+sudo pacman -S xorg xorg-xinit pulseaudio pulseaudio-alsa
 sudo pacman -S bspwm sxhkd
 
 git clone https://aur.archlinux.org/yay.git
@@ -12,9 +12,10 @@ cd yay
 makepkg -si
 cd ..
 
-sudo pacman -S rxvt-unicode firefox thunar feh
+sudo pacman -S rxvt-unicode firefox thunar feh ibus ibus-anthy
 sudo pacman -S adobe-source-han-serif-jp-fonts papirus-icon-theme gtk2 gtk3 imagemagick
 sudo pacman -S network-manager-applet
+sudo pacman -S rofi picom
 
 yay polybar
 yay ttf-iosevka
@@ -22,19 +23,20 @@ yay pa-applet
 
 pip3 install pywal
 
+mkdir ~/.config/rofi
 mkdir ~/.config/sxhkd
 mkdir ~/.config/bspwm
 mkdir ~/.config/polybar
-mkdir "~/.config/gtk-2.0"
-mkdir "~/.config/gtk-3.0"
+mkdir ~/.config/gtk-2.0
+mkdir ~/.config/gtk-3.0
+mkdir ~/.config/wal
+mkdir ~/.config/wal/templates
 
-cp sxhkd/sxhkdrc ~/.config/sxhkdrc
-cp bspwm/bspwmrc ~/.config/bspwmrc
+cp config.rasi ~/.config/rofi/config.rasi
+cp sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 cp polybar/config ~/.config/polybar/config
 cp polybar/launch.sh ~/.config/polybar/launch.sh
-cp "gtk-2.0/gtkfilechooser.ini" "~/.config/gtk-2.0/gtkfilechooser.ini"
-cp "gtk-3.0/settings.ini" "~/.config/gtk-3.0/settings.ini"
-
-chmod +x ~/.config/sxhkd/sxhkdrc
-chmod +x ~/.config/bspwm/bspwmrc
-chmod +x ~/.config/polybar/lunach.sh
+cp gtk-2.0/gtkfilechooser.ini ~/.config/gtk-2.0/gtkfilechooser.ini
+cp gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+cp wal/colors-rofi-dark.rasi ~/.config/wal/templates
